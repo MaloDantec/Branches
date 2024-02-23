9c5aaa67970261a065d7237937032fd8e6807ccf
Malo DANTEC <mdantec56@gmail.com>
2024-02-23 16:41:40 +0100

    GSON

--- box/Box.java
+++ box/Box.java
@@ -1,26 +1,164 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
-import java.util.List;
+import com.google.gson.Gson;
 
-public class Box {
+class Thing{
 
-    private String nomBox;
-    private List<Object> contenu;
+    private String name;
+    private int volume;
 
-    public Box(String nomBox) {
-        this.nomBox = nomBox;
-        this.contenu = new ArrayList<>();
+    public Thing(String name){
+        this.name = name;
+        this.volume = 0;
     }
 
-    public String getNom() {
-        return this.nomBox;
+    public Thing(String name, int volume){
+        this.name = name;
+        this.volume = volume;
     }
 
-    public void ajouterContenu(Object o) {
-        this.contenu.add(o);
+    public String getName() {
+        return this.name;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean hasName(String name) {
+        return this.name == name;
     }
 
     @Override
-    public String toString(){
-        return "La boite "+this.getNom()+" contient "+this.contenu.size()+"objet(s)";
+    public String toString() {
+        return this.name;
     }
 }
+
+public class Box {
+
+    private ArrayList<Thing> contents = new ArrayList<>();
+    private String name;
+    private boolean ouverte;
+    private int capacity;
+
+    public Box(String name){
+        this.name = name;
+        this.contents = new ArrayList<>();
+        this.ouverte = false;
+        this.capacity = -1;
+        System.out.println("Box créée");
+    }
+
+    public Box(String name, int capacity){
+        this.name = name;
+        this.contents = new ArrayList<>();
+        this.ouverte = false;
+        this.capacity = capacity;
+        System.out.println("Box créée");
+    }
+
+    public Box(String name, int capacity, boolean ouverte){
+        this.name = name;
+        this.contents = new ArrayList<>();
+        this.ouverte = ouverte;
+        this.capacity = capacity;
+        System.out.println("Box créée");
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+    
+    public void add(Thing truc) {
+        this.contents.add(truc);
+    }
+
+    public boolean estDedans(Thing thing) {
+        return this.contents.contains(thing);
+    }
+
+    public void enleverDeLaBoite(Thing thing) {
+        try {
+            this.contents.remove(thing);
+        } catch (Exception e) {
+            System.err.println("L'objet n'est pas dans la boite");
+        }
+    }
+
+    public boolean isOpen() {
+        return this.ouverte;
+    }
+
+    public void open() {
+        this.ouverte = true;
+    }
+
+    public void close() {
+        this.ouverte = false;
+    }
+
+    public boolean hasRoomFor(Thing t) {
+        if(this.getCapacity() == -1) {
+            return true;
+        }
+        else{
+            if(t.getVolume() <= this.getCapacity()){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+
+    public void actionAdd(Thing t){
+        if(this.hasRoomFor(t) && this.isOpen()){
+            this.add(t);
+            this.setCapacity(this.getCapacity()-t.getVolume());
+        }
+    }
+
+    public Thing find(String name) {
+        if(this.isOpen()) {
+            for(Thing t : this.contents) {
+                if(t.hasName(name)){
+                    return t;
+                }
+            }
+        }
+        return null;
+        
+    }
+
+    public String actionLook() {
+        String listString = "";
+        for(Thing t: this.contents) {
+            listString += t.toString()+", ";
+        }
+        // listString.substring(0, listString.length() - 5);
+        return "la boite contient: "+listString;
+    }
+
+    public static Box fromJSON() throws FileNotFoundException {
+        FileReader reader = new FileReader("./boite.json");
+        Gson gson = new Gson();
+        Box box = gson.fromJson(reader, Box.class);
+        return box;
+        
+    }
+}
+
---
+++ box/TestsBoxes.java
@@ -0,0 +1,24 @@
+// import org.junit.*;
+// import static org.junit.Assert.assertEquals;
+
+public class TestsBoxes{
+    @Test
+    public void testBoxCreate() {
+        Box b = new Box("test");
+    }
+    /** on veut pouvoir mettre des trucs dedans */
+    @Test
+    public void testBoxAdd(){
+    Box b = new Box("test");
+    b.add(new Thing("truc1"));
+    b.add(new Thing("truc2"));
+
+    // System.out.println(b.actionLook());
+    public static void main(String[] args) {
+        Box j = Box.fromJSON();
+    j.add(new Thing("truc1"));
+
+    System.out.println(j);
+    }
+    
+};
