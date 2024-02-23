8c306e239a6134824a4a3048874b8121942c9d64
Malo DANTEC <mdantec56@gmail.com>
2024-02-23 16:42:12 +0100

    Test

--- box/TestsBoxes.java
+++ box/TestsBoxes.java
@@ -9,10 +9,10 @@ public class TestsBoxes{
     /** on veut pouvoir mettre des trucs dedans */
     @Test
     public void testBoxAdd(){
-    Box b = new Box("test");
-    b.add(new Thing("truc1"));
-    b.add(new Thing("truc2"));
-
+        Box b = new Box("test");
+        b.add(new Thing("truc1"));
+        b.add(new Thing("truc2"));
+    }
     // System.out.println(b.actionLook());
     public static void main(String[] args) {
         Box j = Box.fromJSON();
